f0736aa2893bcaabd1704a17577cc3c51f6debd6
aniksahapritom <aniksaha.math47@gmail.com>
2020-04-30 16:34:22 +0600

    first commit

---
+++ 1.veriables.php
@@ -0,0 +1,9 @@
+<?php
+$txt = "I love my country.";
+$txt2 = "I live at Sherpur in Bangladesh.";
+
+$age = 25;
+
+$addingTwoText = $txt ." ". $txt2 . " Now my age is $age. I have to spent time with my family"; //adding two text here
+
+echo $addingTwoText;
